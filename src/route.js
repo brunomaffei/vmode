137558617424f61e7bac10d2aa2141af9366ea8c
ROTA - CAIXA <bruno.maffei@hotmail.com>
2020-09-03 13:32:37 -0300

    Subindo Projeto

---
+++ src/route.js
@@ -0,0 +1,24 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProgressBar from './components/circularProgress/ProgressBar';
+
+const Login = lazy(() => import('./view/login/login'));
+
+const Routes = () => (
+  <Router>
+    <Suspense
+      fallback={
+        <div className="mt-5 pt-5">
+          <ProgressBar />
+        </div>
+      }
+    >
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Login} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
+
+export default Routes;
